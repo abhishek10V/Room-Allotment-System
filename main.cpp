0fc2743398485192f5fe5396c6f7e93410f7cf30
Abhishek Varshney <mnra1529@gmail.com>
2024-07-27 19:36:27 +0530

    Search and Update functionalities added

--- main.cpp
+++ main.cpp
@@ -21,6 +21,68 @@ class Room{
       void show(); 
 };
 
+void Room::update(){
+    cout<<"\n\t _____________Room Allotment System_____________\n"<<endl;
+    int update_id;
+
+    if(head == NULL){
+       cout<<"\nNo Room available for updating, please first book a Room!!!"<<endl;
+    }else{
+        cout<<"\nEnter the ID of the Room you want to update the details of :"<<endl;
+        cin>>update_id;
+
+        Node * ptr = head;
+        bool flag = false;
+        while(ptr != NULL){
+            if(ptr->id == update_id){
+                flag = true;
+                cout<<"\n\n Enter Room ID : "<<endl;
+                cin>>ptr->id;
+                cout<<"\n Enter Customer Name : "<<endl; 
+                cin>>ptr->name;
+                cout<<"\n Enter Room Allocated Date : "<<endl; 
+                cin>>ptr->date;
+                cout<<"\n Enter Room Type : "<<endl; 
+                cin>>ptr->roomType;
+
+                cout<<"\n\n Room Details Updated Successfully!!!"<<endl;
+            }else{
+                ptr = ptr->next;
+            }
+        }
+        if(flag == false) cout<<"\n\nNo room available for the entered Room ID\n"<<endl;
+        
+    }
+}
+
+void Room::search(){
+    cout<<"\n\t _____________Room Allotment System_____________\n"<<endl;
+    int search_id;
+
+    if(head == NULL){
+       cout<<"\nNo Room available for searching, please first book a Room!!!"<<endl;
+    }else{
+        cout<<"\nEnter the ID of the Room you want to find the details of :"<<endl;
+        cin>>search_id;
+
+        Node * ptr = head;
+        bool flag = false;
+        while(ptr != NULL){
+            if(ptr->id == search_id){
+                flag = true;
+                cout<<"\n\n Room ID : "<<ptr->id<<endl;
+                cout<<"\n Customer Name : "<<ptr->name<<endl; 
+                cout<<"\n Room Allocated Date : "<<ptr->date<<endl; 
+                cout<<"\n Room Type : "<<ptr->roomType<<endl; 
+            }else{
+                ptr = ptr->next;
+            }
+        }
+        if(flag == false) cout<<"\n\nNo room available for the entered Room ID\n"<<endl;
+        
+    }
+}
+
 void Room::insert(){
     cout<<"\n\t _____________Room Allotment System_____________\n"<<endl;
     Node * newNode = new Node;
